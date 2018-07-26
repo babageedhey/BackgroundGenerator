285d71f89417fea8b539c257e98238599ac057ee
babageedhey <babageedhey@gmail.com>
2018-07-26 10:41:22 +0100

    all files

---
+++ script.js
@@ -0,0 +1,13 @@
+var colorName = document.querySelector('h3');
+var color1 = document.querySelector('.color1');
+var color2 = document.querySelector('.color2');
+var body = document.getElementById('gradient');
+
+function setGradient() {
+    body.style.background = "linear-gradient(to right, " + color1.value + "," +  color2.value + ")";
+    colorName.textContent = body.style.background;
+}
+
+color1.addEventListener('input', setGradient);
+
+color2.addEventListener('input', setGradient);
